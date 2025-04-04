f3fc189dfc14ea791017aa5e11fa3950cb2e9fb5
awbasit <nanaqwadjobarima@gmail.com>
2025-04-04 11:19:20 +0000

    Created standard format for unit testing

---
+++ Stock_market_analysis/src/test_stock.py
@@ -0,0 +1,16 @@
+import unittest 
+import pandas as pd
+
+class TestStockFunctions(unittest.TestCase):
+    def test_clean_data(self):
+        pass
+
+    def test_moving_average(self):
+        pass
+
+    def test_insufficient_moving_average(self):
+        pass
+    
+
+if __name__=='__main':
+    unittest.main()
