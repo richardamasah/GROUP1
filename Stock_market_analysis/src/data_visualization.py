bde5db5e81364979ceb199b14ce4fc280db0727e
djanie <richard.amasah@amalitechtraining.org>
2025-04-04 12:38:12 +0000

    preprocess data

--- Stock_market_analysis/src/data_visualization.py
+++ Stock_market_analysis/src/data_visualization.py
@@ -6,3 +6,9 @@ import seaborn as sns
 # Load the dataset
 def load_stock_data(path):
     return pd.read_csv(path)
+
+# Clean & preprocess the dataset
+def preprocess_data(df):
+    df['Date'] = pd.to_datetime(df['Date'])
+    df = df.sort_values('Date')
+    return df
